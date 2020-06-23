bbed805d72e8d4a2cb0c8cada6bedd0af08beb98
Alan Cleetus <alanclee95@gmail.com>
2020-06-22 23:04:00 -0700

    completed project

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,93 @@
 import 'package:flutter/material.dart';
 
+//for hot reload to work, our widgets must be inside stateless
+//or stateful widget
 void main() {
-  runApp(
-    MaterialApp(
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.teal,
-        body: Container(),
+        body: SafeArea(
+            child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            CircleAvatar(
+              radius: 50.0,
+              backgroundImage: NetworkImage(
+                  'https://alancleetus.github.io/assets/images/square_profile_pic.jpg'),
+            ),
+            Text(
+              'Alan Cleetus',
+              style: TextStyle(
+                fontSize: 40.0,
+                color: Colors.white,
+                fontWeight: FontWeight.bold,
+                fontFamily: 'Pacifico',
+              ),
+            ),
+            Text(
+              'SOFTWARE DEVELOPER',
+              style: TextStyle(
+                fontSize: 20.0,
+                color: Colors.teal[100],
+                fontWeight: FontWeight.bold,
+                fontFamily: 'SourceSansPro',
+                letterSpacing: 2.5,
+              ),
+            ),
+            SizedBox(
+              width: 150.0,
+              height: 20.0,
+              child: Divider(),
+            ),
+            Card(
+              margin: EdgeInsets.symmetric(
+                vertical: 10.0,
+                horizontal: 25.0,
+              ),
+              child: ListTile(
+                leading: Icon(
+                  Icons.phone,
+                  color: Colors.teal.shade900,
+                ),
+                title: Text(
+                  '+1 234 657 7890',
+                  style: TextStyle(
+                    color: Colors.teal[900],
+                    fontFamily: 'SourceSansPro',
+                    fontSize: 20.0,
+                  ),
+                ),
+              ),
+            ),
+            Card(
+              margin: EdgeInsets.symmetric(
+                vertical: 10.0,
+                horizontal: 25.0,
+              ),
+              child: ListTile(
+                leading: Icon(
+                  Icons.email,
+                  color: Colors.teal.shade900,
+                ),
+                title: Text(
+                  'alan@email.com',
+                  style: TextStyle(
+                    color: Colors.teal[900],
+                    fontFamily: 'SourceSansPro',
+                    fontSize: 20.0,
+                  ),
+                ),
+              ),
+            )
+          ],
+        )),
       ),
-    ),
-  );
+    );
+  }
 }
